94949d0c61c5bd737f4621ad01581bbf690a9503
JP Laudani <juan.laudani@globant.com>
2022-08-04 18:54:18 -0300

    set up

---
+++ 02_compute_engine/terraform.tfvars
@@ -0,0 +1,7 @@
+project_id   = "panw-355016"
+service_name = "terraform"
+region       = "us-central1"
+zone         = "us-central1-a"
+environment  = "dev"
+tags         = ["alice", "bob"]
+
---
+++ 02_compute_engine/variables.tf
@@ -0,0 +1,24 @@
+variable "project_id" {
+  type = string
+}
+
+variable "service_name" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
+variable "zone" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "tags" {
+  type = list(string)
+
+}
